dee76a65f1868cdc38e33d27e453b3ab2f428676
Tyler Redzko <tylerredzko@gmail.com>
2016-02-27 14:14:12 -0500

    Remove unusable stitch code
    Add gitignore, modify enhancer to re-add color to enhanced image.

--- enhancer.py
+++ enhancer.py
@@ -4,7 +4,10 @@ def clean_whiteboard(infile, outfile):
     import cv2
     import numpy as np
 
-    image = cv2.imread(infile, cv2.IMREAD_GRAYSCALE)
+    color = cv2.imread(infile)
+
+    
+    image = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
 
     # Blur the image using Gaussian filtering
     blur = cv2.GaussianBlur(image, (9, 9), 0)
@@ -14,9 +17,18 @@ def clean_whiteboard(infile, outfile):
         image.astype(np.uint8), 255,
         cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
         cv2.THRESH_BINARY, 71, 10)
+        
+    # filtered does not have depth, but we need it to match
+    # the matrix size for color for addWeighted.
+    # WOULD LIKE A WAY TO AVOID WRITING AND REREADING.
+    cv2.imwrite('filtered.jpg', filtered)
+    filtered = cv2.imread('filtered.jpg')
 
+    # Weight against the original to add color back to image.
+    enh = cv2.addWeighted(color,0.6,filtered,0.4,0)
+    
     # Save the image to disk
-    cv2.imwrite(outfile, filtered)
+    cv2.imwrite(outfile, enh)
 
 
 if __name__=="__main__":
