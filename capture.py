3ac32f3479a24b79079e00adc38386616dca870f
Brian Grohe <grohe43@gmail.com>
2016-02-27 16:14:00 -0500

    Organize image files and include timestamp

--- capture.py
+++ capture.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import os
 
 capture1 = cv2.VideoCapture(1)
 
@@ -8,6 +9,8 @@ if not capture1.isOpened():
 else:
     capture1.set(cv2.CAP_PROP_FRAME_HEIGHT, 1600)
     capture1.set(cv2.CAP_PROP_FRAME_WIDTH, 1200)
+if not os.path.exists("cap/"):
+    os.mkdir("cap/")
 
 print(time.time())
 
@@ -21,6 +24,10 @@ else:
 
 print(time.time())
 
+folder_name = "folder-%d" % time.time()
+
+os.mkdir("cap/" + folder_name)
+
 for n in range(1, 100):
     if capture1:
         read, frame1 = capture1.read()
@@ -29,9 +36,9 @@ for n in range(1, 100):
         read, frame2 = capture2.read()
 
     if capture1:
-        cv2.imwrite("../cap/image1-%d.jpg" % n, frame1)
+        cv2.imwrite("cap/%s/capture1%f.jpg" %  (folder_name, time.time()), frame1)
 
     if capture2:
-        cv2.imwrite("../cap/image2-%d.jpg" % n, frame2)
+        cv2.imwrite("cap/%s/capture2-%f.jpg" % (folder_name, time.time()), frame2)
 
 print(time.time())
