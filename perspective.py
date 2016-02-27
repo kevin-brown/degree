d652d889d59cc0fdddc445b609ebc524eb017930
Gunther Cox <gunthercx@gmail.com>
2016-02-27 14:38:29 -0500

    Moved code to skew correction function in perspective.py

--- perspective.py
+++ perspective.py
@@ -3,9 +3,6 @@ import numpy as np
 
 # Code ripped from http://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
 
-points = {(131, 240), (818, 269), (201, 1105), (660, 1128)}
-points = np.array(list((y, x) for x, y in points))
-
 def order_points(pts):
     # initialzie a list of coordinates that will be ordered
     # such that the first entry in the list is the top-left,
@@ -29,34 +26,48 @@ def order_points(pts):
     # return the ordered coordinates
     return rect
 
-(tl, tr, br, bl) = order_points(points)
 
-image = cv2.imread("img/a.jpg")
+def skew_correction(infile, outfile):
+    points = {(131, 240), (818, 269), (201, 1105), (660, 1128)}
+    points = np.array(list((y, x) for x, y in points))
 
-rect = np.array([tl, tr, br, bl], dtype="float32")
+    (tl, tr, br, bl) = order_points(points)
 
-# compute the width of the new image, which will be the
-# maximum distance between bottom-right and bottom-left
-# x-coordiates or the top-right and top-left x-coordinates
-width_bottom = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-width_top = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-max_width = max(int(width_bottom), int(width_top))
+    image = cv2.imread(infile)
 
-# compute the height of the new image, which will be the
-# maximum distance between the top-right and bottom-right
-# y-coordinates or the top-left and bottom-left y-coordinates
-height_right = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-height_left = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-max_height = max(int(height_right), int(height_left))
+    rect = np.array([tl, tr, br, bl], dtype="float32")
 
-dst = np.array([
-    [0, 0],
-    [max_width - 1, 0],
-    [max_width - 1, max_height - 1],
-    [0, max_height - 1]], dtype = "float32")
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    width_bottom = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    width_top = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    max_width = max(int(width_bottom), int(width_top))
 
-# compute the perspective transform matrix and then apply it
-M = cv2.getPerspectiveTransform(rect, dst)
-warped = cv2.warpPerspective(image, M, (max_width, max_height))
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    height_right = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    height_left = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    max_height = max(int(height_right), int(height_left))
 
-cv2.imwrite("test.jpg", warped)
+    dst = np.array([
+        [0, 0],
+        [max_width - 1, 0],
+        [max_width - 1, max_height - 1],
+        [0, max_height - 1]], dtype = "float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (max_width, max_height))
+
+    cv2.imwrite(outfile, warped)
+
+
+if __name__=="__main__":
+    import sys
+
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+
+    skew_correction(infile, outfile)
