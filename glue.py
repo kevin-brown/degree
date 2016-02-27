e9783cf9d5954461942805889038c92887de326b
Tyler Redzko <tylerredzko@gmail.com>
2016-02-27 15:31:27 -0500

    Fix perspective application.

--- glue.py
+++ glue.py
@@ -8,8 +8,8 @@ def glue_stick(left, right, outfile):
     left = cv2.imread(left)
     right = cv2.imread(right)
     
-    left = imutils.resize(left, height=500)
-    right = imutils.resize(right, height=500)
+    height, _, _ = left.shape
+    right = imutils.resize(right, height=height)
     
     output = np.concatenate((left, right), axis=1)
     
