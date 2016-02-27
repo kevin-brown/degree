e55cd3c659d425449c39ad86a21fc1f86d48cb52
Kevin Brown <kevin@kevin-brown.com>
2016-02-27 10:09:08 -0500

    Notes about where we pulled perspective.py
    Also cut out portions which we don't actually need.

--- perspective.py
+++ perspective.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
 
+# Code ripped from http://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+# TODO: Rewrite this knowing what the points are ahead of time
+# TODO: Get the max height/width knowing that the points aren't reversed
+
 '''
 tl = (300, 115)
 bl = (300, 720)
@@ -15,53 +19,27 @@ tr = (1280, 170)
 
 image = cv2.imread("img/1.jpg")
 
-def order_points(pts):
-    # initialzie a list of coordinates that will be ordered
-    # such that the first entry in the list is the top-left,
-    # the second entry is the top-right, the third is the
-    # bottom-right, and the fourth is the bottom-left
-    rect = np.zeros((4, 2), dtype = "float32")
-
-    # the top-left point will have the smallest sum, whereas
-    # the bottom-right point will have the largest sum
-    s = pts.sum(axis = 1)
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
-
-    # now, compute the difference between the points, the
-    # top-right point will have the smallest difference,
-    # whereas the bottom-left will have the largest difference
-    diff = np.diff(pts, axis = 1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
-
-    # return the ordered coordinates
-    return rect
-
-points = np.array([tl, bl, tr, br])
-
-rect = order_points(points)
-(tl, tr, br, bl) = rect
+rect = np.array([tl, tr, br, bl], dtype="float32")
 
 # compute the width of the new image, which will be the
 # maximum distance between bottom-right and bottom-left
 # x-coordiates or the top-right and top-left x-coordinates
-widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-maxWidth = max(int(widthA), int(widthB))
+width_bottom = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+width_top = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+max_width = max(int(width_bottom), int(width_top))
 
 # compute the height of the new image, which will be the
 # maximum distance between the top-right and bottom-right
 # y-coordinates or the top-left and bottom-left y-coordinates
-heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-maxHeight = max(int(heightA), int(heightB))
+height_right = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+height_left = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+max_height = max(int(height_right), int(height_left))
 
 dst = np.array([
     [0, 0],
-    [maxWidth - 1, 0],
-    [maxWidth - 1, maxHeight - 1],
-    [0, maxHeight - 1]], dtype = "float32")
+    [max_width - 1, 0],
+    [max_width - 1, max_height - 1],
+    [0, max_height - 1]], dtype = "float32")
 
 # compute the perspective transform matrix and then apply it
 M = cv2.getPerspectiveTransform(rect, dst)
